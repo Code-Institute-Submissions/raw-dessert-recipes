8b268e38e2fe10ca3b255532de1aff293c9c39a4
Ann Hayden <hayden.ann@gmail.com>
2018-07-11 11:00:57 +0000

    Add addrecipie.html app.py routing

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ def get_recipes():
     return render_template("recipes.html",
     recipes=mongo.db.recipes.find())
     
+@app.route('add_recipe')
+def add_recipe():
+    '''Routing view to render/call addrecipe.html in browser.'''
+    return render_template('addrecipe.html')
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
