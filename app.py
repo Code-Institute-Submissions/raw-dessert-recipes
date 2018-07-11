89901cb5d2c64f788c1830f0d6a46af062c4ffc3
Ann Hayden <hayden.ann@gmail.com>
2018-07-11 20:06:52 +0000

    Add photo and text with CSS styling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
