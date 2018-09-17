13178cd7cb51cf6502467cb2916174dd8e30ee9e
Ann Hayden <hayden.ann@gmail.com>
2018-09-17 14:20:51 +0000

    Readding Bootstrap template, require wiring up and each page to be built.

--- app.py
+++ app.py
@@ -10,50 +10,12 @@ app.config["MONGO_URI"] = 'mongodb://admin:animals1234@ds233541.mlab.com:33541/r
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/get_recipes')
-def get_recipes():
-    '''Routing view to render/call recipes.html in browser.'''
-    return render_template("recipes.html",
-    recipes=mongo.db.recipes.find())
+def index():
+    return render_template("index.html")
     
-@app.route('/add_recipe')
-def add_recipe():
-    '''Routing view to render/call addrecipe.html in browser.'''
-    return render_template('addrecipe.html')
-    
-@app.route('/insert_recipe', methods=['POST'])
-def insert_recipe():
-    '''Routing to add new recipe.'''
-    recipes =  mongo.db.recipes
-    recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipes'))
-    
-@app.route('/edit_recipe/<recipe_id>')
-def edit_recipe(recipe_id):
-    '''Routing to edit recipe when user selects edit button.'''
-    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template('editrecipe.html', recipe=the_recipe)
-    
-@app.route('/update_recipe/<recipe_id>', methods=["POST"])
-def update_recipe(recipe_id):
-    '''Routing to include recipe prior data within edit view.'''
-    recipes = mongo.db.recipes
-    recipes.update( {'_id': ObjectId(recipe_id)},
-    {
-        'recipe_title':request.form.get['recipe_title'],
-        'date_posted':request.form.get['date_posted'],
-        'description':request.form.get['description'],
-        'ingredients':request.form.get['ingredients'],
-        'instructions':request.form.get['instructions'],
-        'author_name':request.form.get['author_name']
-    })
-    return redirect(url_for('get_recipes'))
-    
-@app.route('/delete_recipe/<recipe_id>')
-def delete_recipe(recipe_id):
-    '''Routing to delete recipe when user selects delete button'''
-    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
-    return redirect(url_for('get_recipes'))
+@app.route('/about')
+def about():
+    return render_template("about.html")
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
