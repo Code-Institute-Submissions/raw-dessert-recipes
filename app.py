b895b0133473d812f5797dc388f19190a6f1c7a5
Ann Hayden <hayden.ann@gmail.com>
2018-09-24 14:09:09 +0000

    Added editrecipe.html blank template.

--- app.py
+++ app.py
@@ -31,26 +31,17 @@ def view_recipe(recipe_id):
     all_categories=mongo.db.categories.find()
     return render_template('viewrecipe.html', recipe=the_recipe, categories=all_categories)
 
-
-
-
-
-
-@app.route('/add_recipe')
-def add_recipe():
-    '''Routing view to render/call addrecipe.html in browser.'''
-    return render_template('addrecipe.html')
-    
-
-
-
-@app.route('/insert_recipe', methods=['POST'])
-def insert_recipe():
-    '''Routing to add new recipe.'''
-    recipes =  mongo.db.recipes
-    recipes.insert_one(request.form.to_dict())
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    '''Routing to delete recipe when user selects delete button'''
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
-    
+
+
+
+
+
+
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     '''Routing to edit recipe when user selects edit button.'''
@@ -71,18 +62,32 @@ def update_recipe(recipe_id):
         'author_name':request.form.get['author_name']
     })
     return redirect(url_for('get_recipes'))
+
+
+
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    '''Routing view to render/call addrecipe.html in browser.'''
+    return render_template('addrecipe.html')
     
-@app.route('/delete_recipe/<recipe_id>')
-def delete_recipe(recipe_id):
-    '''Routing to delete recipe when user selects delete button'''
-    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    '''Routing to add new recipe.'''
+    recipes =  mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
+    
+
+    
+
     
 @app.route('/contact')
 def contact():
     '''Routing view to render/call contact.html in browser'''
     return render_template("contact.html")  
-    
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
