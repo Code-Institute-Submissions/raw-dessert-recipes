ae2391f5a0cb5fd8a63ec62c787ca5600df08d4b
Ann Hayden <hayden.ann@gmail.com>
2018-09-26 19:19:03 +0000

    Updated buttons with Javascript alerts, to verify that user would like to add, edit, delete a recipe.

---
+++ static/js/custom.js
@@ -0,0 +1,11 @@
+function addRecipe() {
+    alert("Are you sure you're ready to add this new recipe?");
+}
+
+function editRecipe() {
+    alert("Are you sure you're ready to add this new recipe? Don't forget to delete the old version of this recipe!");
+}
+
+function deleteRecipe() {
+    alert("Are you sure you'd like to delete this recipe forever?");
+}
