60e3d2c6a286197a3725b6561920db773981e65d
Ann Hayden <hayden.ann@gmail.com>
2018-09-28 17:05:00 +0000

    Finalising README, spellcheck and final end to end check of project outstanding.

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,10 +2,5 @@
 
 // JS for alert box to display when a user selects 'Add Recipe'. //
 function editRecipe() {
-    alert("Are you sure you're ready to add a new version of this recipe? Don't forget to delete the old version!");
-}
-
-// JS for alert box to display when a user selects 'Delete Forever'. //
-function deleteRecipe() {
-    alert("Are you sure you'd like to delete this recipe forever?");
+    alert("Don't forget to delete the old version of this recipe!");
 }
